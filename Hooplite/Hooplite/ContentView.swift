595d135f4b25ab9712724804b964171be6133918
Jeffrey Santana <jsantana@watsco.com>
2024-12-30 05:05:41 -0500

    Created project

---
+++ Hooplite/Hooplite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hooplite
+//
+//  Created by Jeffrey Santana on 12/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hooplite/Hooplite/HoopliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopliteApp.swift
+//  Hooplite
+//
+//  Created by Jeffrey Santana on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct HoopliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
